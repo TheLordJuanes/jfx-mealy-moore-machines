8add58153a9ed5c91378621b087025cbf0b1936a
Carlos Jimmy Pantoja Cadavid <cadavid1230@gmail.com>
2022-02-27 21:28:15 -0500

    UI Improve

--- src/ui/MainGUI.java
+++ src/ui/MainGUI.java
@@ -3,6 +3,8 @@ package ui;
 import com.jfoenix.controls.JFXRadioButton;
 import com.jfoenix.controls.JFXTextField;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.HPos;
@@ -11,49 +13,63 @@ import javafx.geometry.Pos;
 import javafx.geometry.VPos;
 import javafx.scene.control.Control;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 
 public class MainGUI {
 
-    @FXML
-    private JFXTextField nStates;
+	@FXML
+	private JFXTextField nStates;
 
-    @FXML
-    private JFXTextField alphabet;
-    
-    @FXML
-    private HBox tSpace;
-    
-    @FXML
-    private GridPane matrixInput;
-    
-    @FXML
-    private GridPane matrixOutput;
-    
-    @FXML
-    private JFXRadioButton mealy;
+	@FXML
+	private JFXTextField alphabet;
 
-    @FXML
-    private JFXRadioButton moore;
-    
-    @FXML
-    public void configMealy(ActionEvent event) {
-    	moore.setSelected(false);
-    }
-    
-    @FXML
-    public void configMoore(ActionEvent event) {
-    	mealy.setSelected(false);
-    }
-    
-    @FXML
-    public void gTable(ActionEvent event) {
-    	int n = Integer.parseInt(nStates.getText());
-    	String[] alph = alphabet.getText().split(",");
-    	matrixInput = new GridPane();
-    	matrixInput.setAlignment(Pos.CENTER);
-		
+	@FXML
+	private JFXTextField separator;
+
+	@FXML
+	private HBox tSpace;
+
+	@FXML
+	private GridPane matrixInput;
+
+	@FXML
+	private GridPane matrixOutput;
+
+	@FXML
+	private JFXRadioButton mealy;
+
+	@FXML
+	private JFXRadioButton moore;
+
+	@FXML
+	public void configMealy(ActionEvent event) {
+		moore.setSelected(false);
+	}
+
+	@FXML
+	public void configMoore(ActionEvent event) {
+		mealy.setSelected(false);
+	}
+
+	@FXML
+	public void statesIsNumber(KeyEvent event) {
+		if(!Character.isDigit(event.getCharacter().charAt(0))) {
+			event.consume();
+		}
+	}
+
+	@FXML
+	public void gTable(ActionEvent event) {
+		if(!mealy.isSelected()&&!moore.isSelected()) {
+			return;
+		}
+		int n = Integer.parseInt(nStates.getText());
+		String[] alph = alphabet.getText().split(separator.getText());
+		matrixInput = new GridPane();
+		matrixInput.setAlignment(Pos.CENTER);
+
 		placeInMatrix(new Label(""), matrixInput, 0, 0);
 		for(int i=0; i<n; i++) {
 			placeInMatrix(new Label(((char)(65+i))+""), matrixInput, 0, i+1);
@@ -66,17 +82,83 @@ public class MainGUI {
 			placeInMatrix(new Label("S"), matrixInput, cols+1, 0);
 			cols++;
 		}
-		for(int i=1; i<=n; i++) {
-			for(int j=1; j<=cols; j++) {
-				placeInMatrix(new JFXTextField(""), matrixInput, j, i);
+		if(mealy.isSelected()) {
+			for(int i=1; i<=n; i++) {
+				for(int j=1; j<=cols; j++) {
+					JFXTextField aux = new JFXTextField(",");
+					aux.focusedProperty().addListener(new ChangeListener<Boolean>(){
+						@Override
+						public void changed(ObservableValue<? extends Boolean> arg0, Boolean oldPropertyValue, Boolean newPropertyValue){
+							if (!newPropertyValue){
+								if(aux.getText()!=null && aux.getText().length()>=3 && (','==aux.getText().charAt(1)) && 
+										('0'==aux.getText().charAt(2)||'1'==aux.getText().charAt(2))) {
+									boolean ok = false;
+									for(int i=0; i<n&&!ok; i++) {
+										if(65+i==aux.getText().charAt(0)) {
+											ok = true;
+										}
+									}
+									if(!ok) {
+										aux.setText(",");
+									}
+								} else {
+									aux.setText(",");
+								}
+							}
+						}
+					});
+					placeInMatrix(aux, matrixInput, j, i);
+				}
+			}
+		} else {
+			for(int i=1; i<=n; i++) {
+				for(int j=1; j<=cols; j++) {
+					JFXTextField aux = new JFXTextField("");
+					if(j==cols) {
+						aux.focusedProperty().addListener(new ChangeListener<Boolean>(){
+							@Override
+							public void changed(ObservableValue<? extends Boolean> arg0, Boolean oldPropertyValue, Boolean newPropertyValue){
+								if (!newPropertyValue){
+									if(aux.getText()!=null && aux.getText().length()==1 && ('0'==aux.getText().charAt(0)||'1'==aux.getText().charAt(0))) {
+										
+									} else {
+										aux.setText("");
+									}
+								}
+							}
+						});
+					} else {
+						aux.focusedProperty().addListener(new ChangeListener<Boolean>(){
+							@Override
+							public void changed(ObservableValue<? extends Boolean> arg0, Boolean oldPropertyValue, Boolean newPropertyValue){
+								if (!newPropertyValue){
+									if(aux.getText()!=null && aux.getText().length()==1) {
+										boolean ok = false;
+										for(int i=0; i<n&&!ok; i++) {
+											if(65+i==aux.getText().charAt(0)) {
+												ok = true;
+											}
+										}
+										if(!ok) {
+											aux.setText("");
+										}
+									} else {
+										aux.setText("");
+									}
+								}
+							}
+						});
+					}
+					placeInMatrix(aux, matrixInput, j, i);
+				}
 			}
 		}
 		tSpace.getChildren().remove(0);
-    	tSpace.getChildren().add(0, matrixInput);
-    	matrixInput.setVisible(true);
-    }
-    
-    /*private void experimental() {
+		tSpace.getChildren().add(0, matrixInput);
+		matrixInput.setVisible(true);
+	}
+
+	/*private void experimental() {
     	int n = 9;
     	String[] alph = {"0", "1"};
     	String[] A = {"A", "I", "C", "1"};
@@ -99,8 +181,8 @@ public class MainGUI {
     		}
     	} 
     }*/
-    
-    private <Type extends Control> void placeInMatrix(Type element, GridPane matrix, int column, int row) {
+
+	private <Type extends Control> void placeInMatrix(Type element, GridPane matrix, int column, int row) {
 		matrix.add(element, column, row);
 		GridPane.setHalignment(element, HPos.CENTER);
 		GridPane.setValignment(element, VPos.CENTER);
